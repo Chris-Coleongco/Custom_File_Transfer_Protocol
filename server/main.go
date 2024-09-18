eeb251cd07082e3f3601e2a25d4fc1fe68cf73a8
Chris-Coleongco <christien.coleongco@gmail.com>
2024-09-17 19:33:42 -0600

    added some flag stuff and a get_file function to read the file in the server-data folder

--- server/main.go
+++ server/main.go
@@ -1,21 +1,43 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
 	"net"
+	"os"
 )
 
 func get_file() {
 	//   read the file chunk by chunk. no need to read the whole file into memory, thats stupid. just read it using a seek thingy
-	awlkergj
-}
 
-func protocol(data []byte) {
-	// the header will contain length
-	// parse length
-	// record length in an int var to know how much data to expect
+	// add concurrent downloads later for multiple at once
 
-	// can reconstruct the whole by knowing the length
+	file, err := os.Open("./server-data/data.mp4")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+
+	buffer := make([]byte, 1024)
+
+	for {
+		n, err := reader.Read(buffer)
+		if err != nil {
+			fmt.Println("err readign file")
+			fmt.Println("err")
+			if err.Error() == "EOF" {
+				break
+			}
+		}
+
+		fmt.Printf("buffer contains %d bytes: %s", n, buffer)
+
+	}
 }
 
 func read_conn(conn net.Conn) ([]byte, error) {
@@ -29,17 +51,40 @@ func read_conn(conn net.Conn) ([]byte, error) {
 	return buffer, nil
 }
 
+func interpret_input(buffer []byte) {
+	// format of incoming data: [header -> (command d or u, length of file in bytes if u), payload -> (file to download)]Accept
+	//
+
+	d := flag.String("d", "", "use -d for downloading from server")
+	u := flag.String("u", "", "use -u for uploading to server")
+
+	flag.Parse()
+
+	download_path := *d
+	upload_path := *u
+
+	if download_path != "" {
+		get_file()
+	}
+
+	if upload_path != "" {
+		// get ready to receive a file
+	}
+}
+
 func handle_connection(conn net.Conn) {
 	defer conn.Close()
 
 	// invoke protocol on first read v
-	initial_read, err := read_conn(conn)
+	read, err := read_conn(conn)
 	if err != nil {
 		fmt.Println("initial read failed")
 		fmt.Println(err)
 		return
 	}
-	protocol(initial_read)
+
+	interpret_input(read)
+
 	// the initial read needs to invoke the protocol
 }
 
